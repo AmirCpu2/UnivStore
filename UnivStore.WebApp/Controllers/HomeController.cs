d13f7d8d385ecc02222b53be827a02abf7d8b404
Your Name <yourmail@mail.com>
2020-01-02 00:58:15 +0330

    Creat CSS and JS Files , Creat HomeView and Layout , Partials

--- UnivStore.WebApp/Controllers/HomeController.cs
+++ UnivStore.WebApp/Controllers/HomeController.cs
@@ -1,30 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace UnivStore.WebApp.Controllers
+namespace univStore.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /Home/
+        
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
